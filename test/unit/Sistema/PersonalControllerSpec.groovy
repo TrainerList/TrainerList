e7e3f0a7a28f12d01d5d50d57a9a8ed39095cde3
carlosBittencourt668 <carlos.668@hotmail.com>
2015-11-29 12:58:56 -0200

    teste Personal

--- test/unit/Sistema/PersonalControllerSpec.groovy
+++ test/unit/Sistema/PersonalControllerSpec.groovy
@@ -14,4 +14,34 @@ class PersonalControllerSpec extends Specification {
         //params["name"] = 'someValidName'
     }
 
+    void "Cadastrar com Sucesso"() {
+
+        when:
+        def resposta
+
+        Personal personal = new Personal()
+
+        personal.nome = nome
+        personal.dataNascimento = dtNasci
+        personal.email = email
+        personal.senha = senha
+
+        personal.validate()
+
+
+        if (!personal.hasErrors())
+        {
+            personal.save(flush: true)
+            resposta = "Salvo com Sucesso"
+        }
+
+        then:
+        resposta == "Salvo com Sucesso"
+
+        where:
+        nome | dtNasci | email | senha
+        "carlos" | new Date() | "carlos.668@hotmail.com" | "12345678"
+        "dias" | new Date() | "carlosDias.668@hotmail.com" | "12345678"
+    }
+
 }
